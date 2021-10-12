8faf738392155c0527090142cde8335dc2e8a551
alejovarcasia <alejovarcasia@gmail.com>
2021-10-12 15:26:33 +0000

    modificando 'views', utilizacion de metodo ajax

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,8 +1,12 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$route['default_controller'] = 'inicio';
+$route['default_controller'] = 'welcome';
 $route['404_override'] = 'errors/page_missing';
 $route['translate_uri_dashes'] = FALSE;
-$route['signup'] = 'signup';
-
+$route['/'] = 'welcome/index';
+$route['fetch'] = 'welcome/fetch';
+$route['insert'] = 'welcome/insert';
+$route['delete'] = 'welcome/delete';
+$route['edit'] = 'welcome/edit';
+$route['update'] = 'welcome/update';
